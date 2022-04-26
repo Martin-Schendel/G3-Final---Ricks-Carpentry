ae9b960d141b9bc81014019187ba4bfd63d227f8
zjhellweg <62082086+zjhellweg@users.noreply.github.com>
2022-04-25 21:37:43 -0500

    Update index.php
    Made a change so the doc checks if the user is signed in and links to login if they arn't

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+    session_start();
+    if(isset($_POST['UserName']) && !isset($_SESSION['UserName'])){
+        $_SESSION['UserName'] = $_POST['UserName'];
+    }
+
+    if(!isset($_SESSION['UserName'])){
+        header("Location: login.php");
+        exit();
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
