799a5191d8b47c8b867761994a74d8be528206e7
Martin-Schendel <13mschen@gmail.com>
2022-04-26 21:51:11 -0500

    Yay :)

--- GetCustomerOptions.php
+++ GetCustomerOptions.php
@@ -7,9 +7,8 @@ HEREDOC;
 
  $result = $conn->query($query);
  $numRows = $result->num_rows;
- for ($i = 0; $i < $numRows; $i++){
-    $result->data_seek($i);
-    $CustomerID = htmlspecialchars($result->fetch_assoc()['CustomerID']);
-    $CustomerName = htmlspecialchars($result->fetch_assoc()['CustomerName']);
-    echo("<option value=\"$CustomerID\">$CustomerName</option>")
+ while ($row = $result->fetch_assoc()){
+    $CustomerID = htmlspecialchars($row['CustomerID']);
+    $CustomerName = htmlspecialchars($row['CustomerName']);
+    echo("<option value=\"$CustomerID\">$CustomerName</option>");
  }
