62be4e190bc05b1c5f0a0118368477cc68b82986
zjhellweg <62082086+zjhellweg@users.noreply.github.com>
2022-04-28 15:14:23 -0500

    Update CreateOrder.php

--- CreateOrder.php
+++ CreateOrder.php
@@ -3,36 +3,85 @@ session_start();
 require('Config.php');
 
 // Get items
-$stmt = $pdo->prepare('SELECT * FROM Item');
-$stmt->execute();
 
-// The customer clicks to add to cart
-if (isset($_POST['ItemID'], $_POST['quantity']) && is_string($_POST['ItemID']) && is_numeric($_POST['quantity'])) {
-  // Set the post variables so we easily identify them, also make sure they are integer
-  $ItemID = (string)$_POST['ItemID'];
-  $quantity = (int)$_POST['quantity'];
-  // Prepare the SQL statement, checking if the item exists in the database
-  $stmt = $pdo->prepare('SELECT * FROM Item WHERE ItemID = ?');
-  $stmt->execute([$_POST['ItemID']]);
-  // Fetch the item from the database and return the result
-  $ItemName = $stmt->fetch(PDO::FETCH_ASSOC);
-  // Check if the item exists (not empty)
-  if ($ItemName && $quantity > 0) {
-      // Item exists in database, create/update the session variable for the cart
-      if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
-          if (array_key_exists($ItemID, $_SESSION['cart'])) {
-              // Item exists in cart so just update the quanity
-              $_SESSION['cart'][$ItemID] += $quantity;
-          } else {
-              // Item is not in cart so add it
-              $_SESSION['cart'][$ItemID] = $quantity;
-          }
-      } else {
-          // There are no items in cart, this will add the first item to cart
-          $_SESSION['cart'] = array($ItemID => $quantity);
-      }
-  }
+$stmt = $conn->query('SELECT * FROM Item');
+
+$items = array();
+$ids = array();
+$prices = array();
+$created = false;
+$id = 0;
+$total = 0.0;
+$Number = 0;
+
+echo($_POST["Employee"]);
+
+if($stmt->num_rows > 0){
+    
+    while($row = $stmt->fetch_assoc()){
+       $items[$row["ItemName"]] = "itemm" . $row["ItemID"];
+       $ids[$row["ItemName"]] = $row["ItemID"];
+       $prices[$row["ItemName"]] = $row['ItemSalePrice'];
+       echo($row["ItemName"] . " " . $ids[$row["ItemName"]] . "<br>");
+    }
 }
 
+$sql = "Insert into orderitem('ItemId','Quantity','SalePrice','FilledState','OrderID') Values ";
+
+foreach($items as $key => $value){
+    if(isset($_POST[$value])){
+
+        if(!$created){
+            $CreateSummorySql = "Insert into ordersummary(CustomerID,EmployeeId,OrderTotalPrice) "
+            . "Values ( " . $_POST['Customer'] . "," . $_POST['Employee'] . ",100);";
+            
+            $QueryResult = $conn -> query($CreateSummorySql);
+
+            if(!$QueryResult){
+                Echo($CreateSummorySql . "<br>");
+                Echo(mysqli_error($conn));
+            }
+
+            $GetSummorySql = "Select OrderID From ordersummary Where OrderDate =  " . 
+            "(Select Max(OrderDate) from ordersummary)";
+
+            $result = $conn -> query($GetSummorySql);
+
+            if ($result->num_rows > 0){
+                while($row = $result->fetch_assoc()){
+                    $id = $row['OrderID'];
+                }
+            }
+
+            $created=true;
+
+        }
+
+        //checks if the last character in the sql creatiion script is )
+        //if it isn't that means that this isn't the first value and it puts in a comma.
+        if(substr($sql,-1) != ' '){
+            $sql .= ",";
+        }
+        $sql .= "(" . $ids[$key] . ',' . $_POST[$items[$key]] . ',' . $prices[$key] 
+        . ',' . "'Unfilled'" . ',' . $id . ")";
+
+        $total += $_POST[$items[$key]] * $prices[$key];
+    }
+}
+
+$sql .= ";";
+
+$queryCheck = $conn -> query($sql);
+
+if(!$queryCheck){
+    echo($sql);
+    Echo(mysqli_error($conn));
+}
+
+$PriceSql = `UPDATE ordersummary SET OrderTotalPrice = $total WHERE OrderID = $id;`;
+
+$conn -> query($PriceSql);
+
 ?>
+
 
