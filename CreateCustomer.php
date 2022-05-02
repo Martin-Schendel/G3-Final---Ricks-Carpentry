c85bbef06e99aa75908476851d49a538740778c0
darkstar1651 <56945763+darkstar1651@users.noreply.github.com>
2022-05-01 22:04:59 -0500

    Add files via upload
    I got the input validation messages to display when improper data is entered. Also bad data should not upload to database.

--- CreateCustomer.php
+++ CreateCustomer.php
@@ -16,12 +16,8 @@ if (isset($_POST['CustFirstName'])&&
         $stmt = $conn->prepare("INSERT INTO customer VALUES(?,?,?,?,?)");
 
         $stmt->bind_param("issss",$id,$fname,$lname,$email,$phone);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $stmt->close();
-        $conn->close();
- 
-    
+       
+       
 // Defining variables and set to empty values
 $CustFirstNameErr = $CustLastNameErr = $CustEmailErr = $CustPhoneErr = $CustUsernameErr = "";
 $CustFirstName = $CustLastName = $CustEmail = $CustPhone = $CustUsername = "";
@@ -47,49 +43,71 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //     }
 
     if (empty($_POST["CustFirstName"])) {
-        $CustFirstNameErr = "First name is required";
+       // $CustFirstNameErr = "First name is required";
+        header( "Location: Customers.php?name=empty" );
+        exit();
     }
     else {
         $CustFirstName = test_input($_POST["CustFirstName"]);
             // check if name only contains letters
             if (!preg_match("/^[a-zA-Z]*$/",$CustFirstName)) {
-            $CustFirstNameErr = "Only letters allowed";
+            //$CustFirstNameErr = "Only letters allowed";
+            header( "Location: Customers.php?name=numbers" );
+            exit();
             }
         }
     
     if (empty($_POST["CustLastName"])) {
-        $CustLastNameErr = "Last name is required";
+        //$CustLastNameErr = "Last name is required";
+        header( "Location: Customers.php?lastName=empty" );
+        exit();
             }
     else {
         $CustLastName = test_input($_POST["CustLastName"]);
         // check if name only contains letters
             if (!preg_match("/^[a-zA-Z]*$/",$CustLastName)) {
-            $CustLastNameErr = "Only letters allowed";
+            //$CustLastNameErr = "Only letters allowed";
+            header( "Location: Customers.php?lastName=numbers" );
+            exit();
             }
     }
 
     if (empty($_POST["CustEmail"])) {
-        $CustEmailErr = "Email is required";
+        //$CustEmailErr = "Email is required";
+        header( "Location: Customers.php?email=empty" );
+            exit();
         } 
     else {
         $CustEmail = test_input($_POST["CustEmail"]);
         // check if e-mail address is valid
             if (!filter_var($CustEmail, FILTER_VALIDATE_EMAIL)) {
-            $CustEmailErr = "Invalid email format";
+            //$CustEmailErr = "Invalid email format";
+            header( "Location: Customers.php?email=invalid" );
+            exit();
             }
     }
         
     if (empty($_POST["CustPhone"])) {
-        $CustPhone = "";
+        //$CustPhone = "";
+        header( "Location: Customers.php?phone=empty" );
+            exit();
         } 
     else {
         $CustPhone = test_input($_POST["CustPhone"]);
         // check if phone number is valid
             if (!preg_match('/^[0-9]{10}+$/', $CustPhone)) {
-            $CustPhoneErr = "Invalid phone number - Please enter 10 digits";
+            //$CustPhoneErr = "Invalid phone number - Please enter 10 digits";
+            header( "Location: Customers.php?phone=invalid" );
+            exit();
             }
     }
+    //moved down here so if checks fail database will not be updated
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+    $conn->close();
 }
 }
+
 header( "Location: index.php" );
 ?>
--- CreateEmployee.php
+++ CreateEmployee.php
@@ -21,10 +21,7 @@ if (isset($_POST['EmpFirstName'])&&
         $stmt = $conn->prepare("INSERT INTO employee VALUES(?,?,?,?,?,?,?)");
         //flipped password and username around because they were assigning the wrong values in database-ben
        $stmt->bind_param("issssss",$id,$fname,$lname,$email,$phone,$password,$uname);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $stmt->close();
-        $conn->close();
+        
  
     
 // Defining variables and set to empty values
@@ -41,59 +38,98 @@ function test_input($data) {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["EmpFirstName"])) {
-        $EmpFirstNameErr = "First name is required";
+       // $EmpFirstNameErr = "First name is required";
+       header( "Location: Employees.php?name=empty" );
+        exit();
     }
     else {
         $EmpFirstName = test_input($_POST["EmpFirstName"]);
             // check if name only contains letters
             if (!preg_match("/^[a-zA-Z]*$/",$EmpFirstName)) {
-            $EmpFirstNameErr = "Only letters allowed";
+            //$EmpFirstNameErr = "Only letters allowed";
+            header( "Location: Employees.php?name=numbers" );
+            exit();
             }
         }
     
     if (empty($_POST["EmpLastName"])) {
-        $EmpLastNameErr = "Last name is required";
+       // $EmpLastNameErr = "Last name is required";
+       header( "Location: Employees.php?lastName=empty" );
+        exit();
             }
     else {
         $EmpLastName = test_input($_POST["EmpLastName"]);
         // check if name only contains letters
             if (!preg_match("/^[a-zA-Z]*$/",$EmpLastName)) {
-            $EmpLastNameErr = "Only letters allowed";
+            //$EmpLastNameErr = "Only letters allowed";
+            header( "Location: Employees.php?lastName=numbers" );
+            exit();
             }
     }
 
     if (empty($_POST["EmpEmail"])) {
-        $EmpEmailErr = "Email is required";
+        //$EmpEmailErr = "Email is required";
+        header( "Location: Employees.php?email=empty" );
+            exit();
         } 
     else {
         $EmpEmail = test_input($_POST["EmpEmail"]);
         // check if e-mail address is valid
             if (!filter_var($EmpEmail, FILTER_VALIDATE_EMAIL)) {
-            $EmpEmailErr = "Invalid email format";
+            //$EmpEmailErr = "Invalid email format";
+            header( "Location: Employees.php?email=invalid" );
+            exit();
             }
     }
         
     if (empty($_POST["EmpPhone"])) {
-        $EmpPhone = "";
+       // $EmpPhone = "";
+       header( "Location: Employees.php?phone=empty" );
+            exit();
         } 
     else {
         $EmpPhone = test_input($_POST["EmpPhone"]);
         // check if phone number is valid
-            if (!preg_match('/^[0-9]{11}+$/', $EmpPhone)) {
-            $EmpPhoneErr = "Invalid phone number";
+            if (!preg_match('/^[0-9]{10}+$/', $EmpPhone)) {
+           // $EmpPhoneErr = "Invalid phone number";
+           header( "Location: Employees.php?phone=invalid" );
+            exit();
             }
     }
 
     if (empty($_POST["EmpUsername"])) {
-        $EmpUsernameErr = "Username is required";
+       // $EmpUsernameErr = "Username is required";
+       header( "Location: Employees.php?userTag=empty" );
+            exit();
     }
     else {
         $EmpUsernameErr = test_input($_POST["EmpUsername"]);
             // check if name only contains letters
             if (!preg_match("/^[a-zA-Z]*$/",$EmpUsernameErr)) {
-            $EmpUsernameErr = "Only letters allowed";
+            //$EmpUsernameErr = "Only letters allowed";
+            header( "Location: Employees.php?userTag=invalid" );
+            exit();
             }
         }
+
+        //password empty validation
+        if (empty($_POST["EmpPassword"])) {
+            // $EmpFirstNameErr = "First name is required";
+            header( "Location: Employees.php?Password=empty" );
+             exit();
+         }
+        //password match validation
+
+        if($_POST["EmpPassword"] != $_POST["confirmPassword"]){
+            header( "Location: Employees.php?Password=invalid" );
+            exit();
+        }
+        //moved down here so if checks fail database will not be updated
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        $conn->close();
+
 }
 }
 
