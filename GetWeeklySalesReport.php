598336b9678379c408933854a344c98718b4ea2c
zjhellweg <62082086+zjhellweg@users.noreply.github.com>
2022-05-03 13:52:01 -0500

    Updated line 37 to fix errors over 1k

--- GetWeeklySalesReport.php
+++ GetWeeklySalesReport.php
@@ -34,7 +34,7 @@ HEREDOC;
 
     $SaleTotal = htmlspecialchars(number_format((double)$row['Sale Total'],2));
     $Profit = htmlspecialchars(number_format((double)$row['Profit'],2));
-    $WeeklyProfit += $Profit;
+    $WeeklyProfit += (double)$row['Profit'];
     echo(<<<HEREDOC
     <tr>
     <td>$OrderID</td>
